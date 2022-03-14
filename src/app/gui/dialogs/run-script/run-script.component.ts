c6185a33c9f016a5a816867f2f7d1671566050a5
kutay <kutay@msu.edu>
2022-03-14 15:57:40 -0400

    Runscript fixed

--- src/app/gui/dialogs/run-script/run-script.component.ts
+++ src/app/gui/dialogs/run-script/run-script.component.ts
@@ -6,167 +6,169 @@ import {SystemConfigService} from "../../../service/system-config.service";
 import {DataFileConfigComponent} from "../load-new/data-file-config/data-file-config.component";
 
 @Component({
-  selector: 'app-run-script',
-  templateUrl: './run-script.component.html',
-  styleUrls: ['./run-script.component.scss']
+    selector: 'app-run-script',
+    templateUrl: './run-script.component.html',
+    styleUrls: ['./run-script.component.scss']
 })
 export class RunScriptComponent implements OnInit {
-  @ViewChild('liveupdate') private liveUpdateElement: ElementRef;
-  runForm: FormGroup;
-  anlForm: FormGroup;
+    @ViewChild('liveupdate') private liveUpdateElement: ElementRef;
+    runForm: FormGroup;
+    anlForm: FormGroup;
 
-  private runScript : RunScript = undefined;
+    private runScript: RunScript = undefined;
 
-  constructor(private fb: FormBuilder,
-              private scriptService: ScriptService,
-              private sysSvc: SystemConfigService) {
-  }
-
-  ngOnInit() {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const required = Validators.required;
-
-    // Configures the form group, sets the default values, and makes each file required.
-    this.runForm = this.fb.group({
-      command: ["", [required]],
-      args: ["", []],
-      results: ["", []],
-      code: ["",[]],
-      liveResults: ["\n",[]]
-    });
-  }
-
-
-  submitRun() {
-    const command = <string>this.sysSvc.appConfigData.pythonCmd ;
-    const fltStr = "";
-    const remotejsonfld = "";
-
-    const args : string[] = [];
-    args.push(`${<string>this.sysSvc.appConfigData.localNapcomDir}/${<string>this.sysSvc.appConfigData.jobSubmitCmd}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlHost}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlUser}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlNodeCluster}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlPwd}`);
-    args.push(`${<string>this.sysSvc.appConfigData.localNapcomDir}`);
-    args.push(`${<string>this.sysSvc.appConfigData.remoteNapcomDir}`);
-    args.push(`${<string>this.sysSvc.appConfigData.workingDirectory}`);
-    args.push(`${fltStr}`);
-    args.push(`${remotejsonfld}`);
-    args.push(`${<string>this.sysSvc.appConfigData.localUpdapsacDir}`);
-    args.push(`${<string>this.sysSvc.appConfigData.remoteUpdapsacDir}`);
-    args.push(`${<string>this.sysSvc.appConfigData.maxNumberOfRuns}`);
-
-    this.run(command, args);
-  }
-
-  uploadNapcomFiles() {
-    const command = <string>this.sysSvc.appConfigData.pythonCmd ;
-
-    const args : string[] = [];
-    args.push(`${<string>this.sysSvc.appConfigData.localNapcomDir}/${<string>this.sysSvc.appConfigData.sendNapcomCmd}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlHost}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlUser}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlNodeCluster}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlPwd}`);
-    args.push(`${<string>this.sysSvc.appConfigData.localNapcomDir}`);
-    args.push(`${<string>this.sysSvc.appConfigData.remoteNapcomDir}`);
-    args.push(`${<string>this.sysSvc.appConfigData.napcomExt}`);
-    args.push(`${<string>this.sysSvc.appConfigData.workingDirectory}`);
-
-    this.run(command, args);
-  }
-
-  uploadUpdapsFiles() {
-    const command = <string>this.sysSvc.appConfigData.pythonCmd ;
-
-    const args : string[] = [];
-    args.push(`${<string>this.sysSvc.appConfigData.localUpdapsacDir}/${<string>this.sysSvc.appConfigData.sendUpdapsCmd}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlHost}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlUser}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlNodeCluster}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlPwd}`);
-    args.push(`${<string>this.sysSvc.appConfigData.localUpdapsacDir}`);
-    args.push(`${<string>this.sysSvc.appConfigData.remoteUpdapsacDir}`);
-    args.push(`${<string>this.sysSvc.appConfigData.updapsacExt}`);
-
-    this.run(command, args);
-  }
-
-
-  checkConnection() {
-    const command = <string>this.sysSvc.appConfigData.pythonCmd ;
-
-    const args : string[] = [];
-    args.push(`${<string>this.sysSvc.appConfigData.localNapcomDir}/${<string>this.sysSvc.appConfigData.checkConnectionCmd}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlHost}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlUser}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlNodeCluster}`);
-    args.push(`${<string>this.sysSvc.appConfigData.anlPwd}`);
-
-    console.log(command + " "+ args)
-    console.log(`pwd = ${<string>this.sysSvc.appConfigData.anlPwd}`);
-    console.log(this.sysSvc.directoryDefined());
-
-    this.run(command, args);
-  }
-
-
-  runCommand() {
-    // Args can be entered in a comma separated list.  They need to be split and passed as an array for spawn.
-    const args : any[]  = (this.runForm.value["args"] != null && this.runForm.value["args"].length > 0)
-      ? this.runForm.value["args"].split(",")
-      : [];
-
-    this.run(<string>this.runForm.value["command"], args);
-  }
-
-
-  run(command: string, args: any[]) {
-
-    // Create a new instance of the run script
-    this.runScript = this.scriptService.create();
-    this.runScript.run(command, args, (dat: string) => {
-
-      // Callback updates live results
-      const patchVal = <string>this.runForm.value["liveResults"] + dat;
-      this.runForm.patchValue({liveResults: patchVal});
-      this.keepAtBottom();
-
-    }).then((dat: ScriptReturn) => {
-
-      // Valid completion displays the results
-      this.runForm.patchValue({results: dat.data});
-      this.runForm.patchValue({code: dat.code});
-
-    }).catch(err => {
-
-      // Error displays the returned error
-      this.runForm.patchValue({results: err});
-      this.runForm.patchValue({code: ""});
-
-    });
-  }
-
-  cancel() {
-    this.runScript.cancel();
-  }
-
-  clear() {
-    this.runForm.patchValue({results: ''});
-    this.runForm.patchValue({code: ''});
-    this.runForm.patchValue({liveResults: ''});
-  }
-
-  onTabChanged(event) {
-    this.clear();
-  }
-
-  keepAtBottom(): void {
-    try {
-      this.liveUpdateElement.nativeElement.scrollTop = this.liveUpdateElement.nativeElement.scrollHeight;
-    } catch(err) {
-      console.log(err);
+    constructor(private fb: FormBuilder,
+                private scriptService: ScriptService,
+                private sysSvc: SystemConfigService) {
     }
-  }
+
+    ngOnInit() {
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        const required = Validators.required;
+
+        // Configures the form group, sets the default values, and makes each file required.
+        this.runForm = this.fb.group({
+            command: ["", [required]],
+            args: ["", []],
+            results: ["", []],
+            code: ["", []],
+            liveResults: ["\n", []]
+        });
+    }
+
+
+    submitRun() {
+        const command = "'" + <string>this.sysSvc.appConfigData.pythonCmd + "'";
+        const fltStr = "";
+        const remotejsonfld = "";
+
+        const args: string[] = [];
+        args.push("'" + `${<string>this.sysSvc.appConfigData.localNapcomDir}/${<string>this.sysSvc.appConfigData.jobSubmitCmd}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlHost}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlUser}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlNodeCluster}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlPwd}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.localNapcomDir}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.remoteNapcomDir}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.workingDirectory}` + "'");
+        args.push("'" + `${fltStr}` + "'");
+        args.push("'" + `${remotejsonfld}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.localUpdapsacDir}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.remoteUpdapsacDir}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.maxNumberOfRuns}` + "'");
+
+        this.run(command, args);
+    }
+
+    uploadNapcomFiles() {
+        const command = "'" + <string>this.sysSvc.appConfigData.pythonCmd + "'";
+
+        const args: string[] = [];
+        args.push("'" + `${<string>this.sysSvc.appConfigData.localNapcomDir}/${<string>this.sysSvc.appConfigData.sendNapcomCmd}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlHost}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlUser}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlNodeCluster}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlPwd}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.localNapcomDir}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.remoteNapcomDir}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.napcomExt}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.workingDirectory}` + "'");
+
+        console.log(command + " " + args)
+
+        this.run(command, args);
+    }
+
+    uploadUpdapsFiles() {
+        const command = "'" + <string>this.sysSvc.appConfigData.pythonCmd + "'";
+
+        const args: string[] = [];
+        args.push("'" + `${<string>this.sysSvc.appConfigData.localNapcomDir}/${<string>this.sysSvc.appConfigData.sendUpdapsCmd}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlHost}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlUser}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlNodeCluster}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlPwd}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.localUpdapsacDir}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.remoteUpdapsacDir}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.updapsacExt}` + "'");
+
+        console.log(command + " " + args)
+
+        this.run(command, args);
+    }
+
+
+    checkConnection() {
+        const command = "'" + <string>this.sysSvc.appConfigData.pythonCmd + "'";
+
+        const args: string[] = [];
+        args.push("'" + `${<string>this.sysSvc.appConfigData.localNapcomDir}/${<string>this.sysSvc.appConfigData.checkConnectionCmd}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlHost}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlUser}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlNodeCluster}` + "'");
+        args.push("'" + `${<string>this.sysSvc.appConfigData.anlPwd}` + "'");
+
+        console.log(command + " " + args)
+
+        this.run(command, args);
+    }
+
+
+    runCommand() {
+        // Args can be entered in a comma separated list.  They need to be split and passed as an array for spawn.
+        const args: any[] = (this.runForm.value["args"] != null && this.runForm.value["args"].length > 0)
+            ? this.runForm.value["args"].split(",")
+            : [];
+
+        this.run(<string>this.runForm.value["command"], args);
+    }
+
+
+    run(command: string, args: any[]) {
+
+        // Create a new instance of the run script
+        this.runScript = this.scriptService.create();
+        this.runScript.run(command, args, (dat: string) => {
+
+            // Callback updates live results
+            const patchVal = <string>this.runForm.value["liveResults"] + dat;
+            this.runForm.patchValue({liveResults: patchVal});
+            this.keepAtBottom();
+
+        }).then((dat: ScriptReturn) => {
+
+            // Valid completion displays the results
+            this.runForm.patchValue({results: dat.data});
+            this.runForm.patchValue({code: dat.code});
+
+        }).catch(err => {
+
+            // Error displays the returned error
+            this.runForm.patchValue({results: err});
+            this.runForm.patchValue({code: ""});
+
+        });
+    }
+
+    cancel() {
+        this.runScript.cancel();
+    }
+
+    clear() {
+        this.runForm.patchValue({results: ''});
+        this.runForm.patchValue({code: ''});
+        this.runForm.patchValue({liveResults: ''});
+    }
+
+    onTabChanged(event) {
+        this.clear();
+    }
+
+    keepAtBottom(): void {
+        try {
+            this.liveUpdateElement.nativeElement.scrollTop = this.liveUpdateElement.nativeElement.scrollHeight;
+        } catch (err) {
+            console.log(err);
+        }
+    }
 }
